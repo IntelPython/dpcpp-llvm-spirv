f19dc54934912b249f997bc6bb0f3725bdba839c
Scherbakov, Denis <denis.scherbakov@intel.com>
2023-06-30 03:35:49 -0500

    aligh conda and wheel build scripts

--- conda-recipe/repack.bat
+++ conda-recipe/repack.bat
@@ -6,28 +6,19 @@ set "BUILD_ARGS=--single-version-externally-managed --record=llvm_spirv_record.t
 
 if not exist %BUILD_PREFIX%\Library\bin-llvm\llvm-spirv.exe (exit 1)
 
+pushd %SRC_DIR%\package
+if not exist %SRC_DIR%\package\dpcpp_llvm_spirv\bin mkdir %SRC_DIR%\package\dpcpp_llvm_spirv\bin
+copy %BUILD_PREFIX%\Library\bin-llvm\llvm-spirv.exe %SRC_DIR%\package\dpcpp_llvm_spirv\bin\
+
 if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
-  pushd %SRC_DIR%\package
-  if not exist %SRC_DIR%\package\dpcpp_llvm_spirv\bin mkdir %SRC_DIR%\package\dpcpp_llvm_spirv\bin
-  copy %BUILD_PREFIX%\Library\bin-llvm\llvm-spirv.exe %SRC_DIR%\package\dpcpp_llvm_spirv\bin\
   %PYTHON% setup.py install %BUILD_ARGS% bdist_wheel
   if errorlevel 1 exit 1
   copy dist\dpcpp_llvm_spirv*.whl %WHEELS_OUTPUT_FOLDER%
   if errorlevel 1 exit 1
-  popd
 ) ELSE (
-  pushd %SRC_DIR%\package
   %PYTHON% setup.py install %BUILD_ARGS%
-  type llvm_spirv_record.txt
-  popd
-
-  pushd %BUILD_PREFIX%\
-  %PYTHON% -c "import dpcpp_llvm_spirv as p; print(p.get_llvm_spirv_path())" > Output
-  set /p DIRSTR= < Output
-
-  copy Library\bin-llvm\llvm-spirv.exe %DIRSTR%
   if errorlevel 1 exit 1
-  del Output
-  popd
+  type llvm_spirv_record.txt
 )
 
+popd
--- conda-recipe/repack.sh
+++ conda-recipe/repack.sh
@@ -14,24 +14,19 @@ echo -e "Inferred DPCPP_LLVM_SPIRV_VERSION=${DPCPP_LLVM_SPIRV_VERSION}"
 BUILD_ARGS="--single-version-externally-managed --record=llvm_spirv_record.txt"
 WHEELS_BUILD_ARGS="-p manylinux2014_x86_64"
 
+pushd $src/package
+echo -e "Start vendoring of llvm-spirv executable \n"
+mkdir -p $src/package/dpcpp_llvm_spirv/bin
+cp ${BUILD_PREFIX}/bin-llvm/llvm-spirv $src/package/dpcpp_llvm_spirv/bin/
+
 if [ -n "${WHEELS_OUTPUT_FOLDER}" ]; then
   # Build wheel package
-  pushd $src/package
-  mkdir -p $src/package/dpcpp_llvm_spirv/bin
-  cp ${BUILD_PREFIX}/bin-llvm/llvm-spirv $src/package/dpcpp_llvm_spirv/bin/
   $PYTHON setup.py install ${BUILD_ARGS} bdist_wheel ${WHEELS_BUILD_ARGS} 
   cp dist/dpcpp_llvm_spirv*.whl ${WHEELS_OUTPUT_FOLDER}
-  popd
 else
   pushd $src/package
   ${PYTHON} setup.py install ${BUILD_ARGS}
   cat llvm_spirv_record.txt
-
-  echo -e "Done building the Python package. Start vendoring of llvm-spirv executable \n"
-
-  pushd ${BUILD_PREFIX}/
-  cp bin-llvm/llvm-spirv $(${PYTHON} -c "import dpcpp_llvm_spirv as p; print(p.get_llvm_spirv_path())")
-  echo "copy llvm-spirv to: $(${PYTHON} -c "import dpcpp_llvm_spirv as p; print(p.get_llvm_spirv_path())")"
-  popd
-  echo "done. \n"
 fi
+echo "done. \n"
+popd
