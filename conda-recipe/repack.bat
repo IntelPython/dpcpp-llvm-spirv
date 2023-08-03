de5531b0f597105105a82dde8477b7dac0e909cb
DenisScherbakov <denis.scherbakov85@gmail.com>
2023-08-03 13:03:39 +0200

    specify plartform for bdist_wheel (#15)

--- conda-recipe/repack.bat
+++ conda-recipe/repack.bat
@@ -13,7 +13,7 @@ copy %BUILD_PREFIX%\Library\bin-llvm\llvm-spirv.exe %SRC_DIR%\package\dpcpp_llvm
 rem Workaround to remove spaces from the env value
 set WHEELS_OUTPUT_FOLDER=%WHEELS_OUTPUT_FOLDER: =%
 if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
-  %PYTHON% setup.py install %BUILD_ARGS% bdist_wheel
+  %PYTHON% setup.py install %BUILD_ARGS% bdist_wheel -p win_amd64
   if errorlevel 1 exit 1
   copy dist\dpcpp_llvm_spirv*.whl %WHEELS_OUTPUT_FOLDER%
   if errorlevel 1 exit 1
