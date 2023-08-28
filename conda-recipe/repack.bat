f9bc1fab1bb5bf7e072d97382de4d4b834191c2e
Scherbakov, Denis <denis.scherbakov@intel.com>
2023-08-28 10:55:41 -0500

    remove dot char from PY_VER

--- conda-recipe/repack.bat
+++ conda-recipe/repack.bat
@@ -13,7 +13,8 @@ copy %BUILD_PREFIX%\Library\bin-llvm\llvm-spirv.exe %SRC_DIR%\package\dpcpp_llvm
 rem Workaround to remove spaces from the env value
 set WHEELS_OUTPUT_FOLDER=%WHEELS_OUTPUT_FOLDER: =%
 if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
-  %PYTHON% setup.py install %BUILD_ARGS% bdist_wheel -p win_amd64 --python-tag py%PY_VER%
+
+  %PYTHON% setup.py install %BUILD_ARGS% bdist_wheel -p win_amd64 --python-tag py%PY_VER:.=%
   if errorlevel 1 exit 1
   copy dist\dpcpp_llvm_spirv*.whl %WHEELS_OUTPUT_FOLDER%
   if errorlevel 1 exit 1
